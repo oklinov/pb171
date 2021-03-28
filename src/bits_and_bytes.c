5f9dce13ac2c10e9d588602d56592f1fa1901dcb
Ondrej Klinovsky <506501@muni.cz>
2021-03-28 18:49:02 +0200

    Implement platform independent functions

--- src/bits_and_bytes.c
+++ src/bits_and_bytes.c
@@ -1,23 +1,34 @@
 #include "bits_and_bytes.h"
 
-int16_t bit(const uint8_t) {
+int16_t bit(const uint8_t n) {
+    return 1 << n;
 }
 
 int16_t bitClear(const int16_t x, const int8_t n) {
+    return x & (0b111111111111111111 ^ (1 << n));
 }
 
 int8_t bitRead(const int16_t x, const int8_t n) {
+    return (x & (1 << n)) >> n;
 }
 
-void bitSet(int16_t x, const int8_t n) {
+void bitSet(int16_t *x, const int8_t n) {
+    *x |= 1 << n;
 }
 
-void bitWrite(int16_t x, const int8_t n, const int8_t b) {
-}
-
-int8_t hightByte(const int16_t x) {
+void bitWrite(int16_t *x, const int8_t n, const int8_t b) {
+    if (b) {
+        bitSet(x, n);
+    } else {
+        *x = bitClear(*x, n);
+    }
 }
 
 int8_t lowByte(const int16_t x) {
+    return (int8_t) x;
+}
+
+int8_t highByte(const int16_t x) {
+    return (int8_t) (x >> 8);
 }
 
--- src/bits_and_bytes.h
+++ src/bits_and_bytes.h
@@ -9,13 +9,13 @@ int16_t bitClear(const int16_t x, const int8_t n);
 
 int8_t bitRead(const int16_t x, const int8_t n);
 
-void bitSet(int16_t x, const int8_t n);
+void bitSet(int16_t *x, const int8_t n);
 
-void bitWrite(int16_t x, const int8_t n, const int8_t b);
-
-int8_t hightByte(const int16_t x);
+void bitWrite(int16_t *x, const int8_t n, const int8_t b);
 
 int8_t lowByte(const int16_t x);
+
+int8_t highByte(const int16_t x);
 
 #endif // BITS_AND_BYTES
 
