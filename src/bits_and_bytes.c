c6ae0e7cdbe703e06cd37db7fd7efffa42ad67b0
Ondrej Klinovsky <506501@muni.cz>
2021-03-28 13:44:05 +0200

    Initial commit

---
+++ src/bits_and_bytes.c
@@ -0,0 +1,23 @@
+#include "bits_and_bytes.h"
+
+int16_t bit(const uint8_t) {
+}
+
+int16_t bitClear(const int16_t x, const int8_t n) {
+}
+
+int8_t bitRead(const int16_t x, const int8_t n) {
+}
+
+void bitSet(int16_t x, const int8_t n) {
+}
+
+void bitWrite(int16_t x, const int8_t n, const int8_t b) {
+}
+
+int8_t hightByte(const int16_t x) {
+}
+
+int8_t lowByte(const int16_t x) {
+}
+
---
+++ src/bits_and_bytes.h
@@ -0,0 +1,21 @@
+#ifndef BITS_AND_BYTES
+#define BITS_AND_BYTES
+
+#include <stdint.h>
+
+int16_t bit(const uint8_t);
+
+int16_t bitClear(const int16_t x, const int8_t n);
+
+int8_t bitRead(const int16_t x, const int8_t n);
+
+void bitSet(int16_t x, const int8_t n);
+
+void bitWrite(int16_t x, const int8_t n, const int8_t b);
+
+int8_t hightByte(const int16_t x);
+
+int8_t lowByte(const int16_t x);
+
+#endif // BITS_AND_BYTES
+
