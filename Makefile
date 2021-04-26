85989360130e20b317f46b344e0128ae29c0538d
Ondrej Klinovsky <506501@muni.cz>
2021-04-26 15:05:09 +0200

    Move the basic functions to header file and make them static inline

--- Makefile
+++ Makefile
@@ -1,11 +1,8 @@
 .PHONY: clean
 
-test: bits_and_bytes.o
-	gcc bits_and_bytes.o tests/bits_and_bytes_test.c -o bits_and_bytes_test
+test: src/bits_and_bytes.h tests/bits_and_bytes_test.c
+	gcc -std=c99 tests/bits_and_bytes_test.c -o bits_and_bytes_test
 	./bits_and_bytes_test
-
-bits_and_bytes.o: src/bits_and_bytes.c
-	gcc -c src/bits_and_bytes.c -o bits_and_bytes.o
 
 clean:
 	rm -f bits_and_bytes.o bits_and_bytes_test
--- src/bits_and_bytes.h
+++ src/bits_and_bytes.h
@@ -3,18 +3,32 @@
 
 #include <stdint.h>
 
-int16_t bit(const uint8_t);
+static inline int16_t bit(const uint8_t n) {
+    return 1 << n;
+}
 
-int16_t bitClear(const int16_t x, const int8_t n);
+static inline int16_t bitClear(const int16_t x, const int8_t n) {
+    return x & (0b111111111111111111 ^ (1 << n));
+}
 
-int8_t bitRead(const int16_t x, const int8_t n);
+static inline int8_t bitRead(const int16_t x, const int8_t n) {
+    return (x & (1 << n)) >> n;
+}
 
-int16_t bitSet(int16_t x, const int8_t n);
+static inline int16_t bitSet(int16_t x, const int8_t n) {
+    return x | 1 << n;
+}
 
-int16_t bitWrite(int16_t x, const int8_t n, const int8_t b);
+static inline int16_t bitWrite(int16_t x, const int8_t n, const int8_t b) {
+    return b ? bitSet(x, n) : bitClear(x, n);
+}
 
-int8_t lowByte(const int16_t x);
+static inline int8_t lowByte(const int16_t x) {
+    return (int8_t) x;
+}
 
-int8_t highByte(const int16_t x);
+static inline int8_t highByte(const int16_t x) {
+    return (int8_t) (x >> 8);
+}
 
 #endif // BITS_AND_BYTES
