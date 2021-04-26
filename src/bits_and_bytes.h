0e0ef7d92735f88e502dd1e7782049ace044998f
Ondrej Klinovsky <506501@muni.cz>
2021-04-26 15:12:03 +0200

    Add documentation to bits_and_bytes functions

--- src/bits_and_bytes.h
+++ src/bits_and_bytes.h
@@ -3,30 +3,51 @@
 
 #include <stdint.h>
 
+/*
+ * returns 2^n
+ */
 static inline int16_t bit(const uint8_t n) {
     return 1 << n;
 }
 
+/*
+ * sets n-th bit to 0 and returns the new value
+ */
 static inline int16_t bitClear(const int16_t x, const int8_t n) {
     return x & (0b111111111111111111 ^ (1 << n));
 }
 
+/*
+ * returns n-th bit of x
+ */
 static inline int8_t bitRead(const int16_t x, const int8_t n) {
     return (x & (1 << n)) >> n;
 }
 
+/*
+ * sets n-th bit of x to 1 and returns the new value
+ */
 static inline int16_t bitSet(int16_t x, const int8_t n) {
     return x | 1 << n;
 }
 
+/*
+ * sets n-th bit of x to b and returns the new value
+ */
 static inline int16_t bitWrite(int16_t x, const int8_t n, const int8_t b) {
     return b ? bitSet(x, n) : bitClear(x, n);
 }
 
+/*
+ * returns lower byte of x
+ */
 static inline int8_t lowByte(const int16_t x) {
     return (int8_t) x;
 }
 
+/*
+ * return higher byte of x
+ */
 static inline int8_t highByte(const int16_t x) {
     return (int8_t) (x >> 8);
 }
