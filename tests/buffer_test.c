75d26981c560f102b4010a163c9d9343d675d750
Ondrej Klinovsky <506501@muni.cz>
2021-06-14 12:14:14 +0200

    Add asserts for buffer's head and tail

--- tests/buffer_test.c
+++ tests/buffer_test.c
@@ -14,37 +14,45 @@ int main(void) {
 
     buffer_store(&buffer, 5);
     assert(buffer_size(&buffer) == 1);
+    assert(buffer.head == 1);
+    assert(buffer.tail == 0);
     assert(buffer_peek(&buffer) == 5);
 
     assert(buffer_load(&buffer) == 5);
+    assert(buffer.tail == 1);
     assert(buffer_size(&buffer) == 0);
     assert(buffer_peek(&buffer) == -1);
 
     buffer_store(&buffer, 1);
     buffer_store(&buffer, 2);
+    assert(buffer.head == 3);
     assert(buffer_size(&buffer) == 2);
     assert(buffer_peek(&buffer) == 1);
     buffer_load(&buffer);
     buffer_load(&buffer);
+    assert(buffer.tail == 3);
 
     for (int i = 0; i <BUFFER_SIZE; i++) {
         buffer_store(&buffer, i);
     }
+    assert(buffer.head == 3);
     assert(buffer_size(&buffer) == BUFFER_SIZE);
 
     buffer_store(&buffer, 'f');
+    assert(buffer.head == 3);
     assert(buffer_size(&buffer) == BUFFER_SIZE);
 
     assert(buffer_load(&buffer) == 0);
     assert(buffer_load(&buffer) == 1);
     assert(buffer_load(&buffer) == 2);
+    assert(buffer.tail == 6);
     assert(buffer_size(&buffer) == BUFFER_SIZE - 3);
 
     buffer_store(&buffer, 'g');
     buffer_store(&buffer, 'h');
     buffer_store(&buffer, 'i');
+    assert(buffer.head == 6);
 
-    printf("size = %d %d\n", buffer_size(&buffer), BUFFER_SIZE);
     assert(buffer_size(&buffer) == BUFFER_SIZE);
 
     printf("ok\n");
